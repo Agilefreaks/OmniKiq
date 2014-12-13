6f5d30b4080ac48e29d4c3f696e85f252fbc0754
adi <nistor.adrian@agilefreaks.com>
2014-12-13 10:58:37 +0200

    Extracted config

---
+++ lib/mixpanel-sidekiq-config.rb
@@ -0,0 +1,18 @@
+module MixpanelSidekiqConfig
+  mattr_accessor :mixpanel_api_key
+  self.mixpanel_api_key = ''
+
+  mattr_accessor :redis_url
+  self.redis_url = 'redis://localhost:6379'
+
+  mattr_accessor :redis_namespace
+  self.redis_namespace = 'default'
+
+
+  mattr_accessor :test_mode
+  self.test_mode = false
+
+  def self.config
+    yield self
+  end
+end
