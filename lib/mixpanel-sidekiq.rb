59095ef889437f9d632d355002aa6c29c6a545d9
adi <nistor.adrian@agilefreaks.com>
2014-12-21 06:05:40 +0200

    Updated gemspec with config file

--- lib/mixpanel-sidekiq.rb
+++ lib/mixpanel-sidekiq.rb
@@ -3,7 +3,6 @@ module MixpanelSidekiq
 
   class MixpanelTracker
     include Sidekiq::Worker
-    sidekiq_options :queue => :default
 
     def perform(email, event_name, params)
       Mixpanel::Tracker.new(MixpanelSidekiq.mixpanel_api_key).track(email, event_name, params)
