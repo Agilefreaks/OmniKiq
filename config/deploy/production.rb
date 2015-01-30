3c62933f104888b6a2bf1954fb8261bd9d20288a
Calin <calinoiu.alexandru@agilefreaks.com>
2015-01-30 12:30:05 +0200

    fixes rubocop issues

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@ set :branch, 'production'
 
 set :rack_env, 'production'
 
-set :default_env, { 'RACK_ENV' => 'production' }
+set :default_env, 'RACK_ENV' => 'production'
 
 role :app, %w(deploy@178.62.253.81)
 role :web, %w(deploy@178.62.253.81)
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@ set :branch, 'staging'
 
 set :rack_env, 'staging'
 
-set :default_env, { 'RACK_ENV' => 'staging' }
+set :default_env, 'RACK_ENV' => 'staging'
 
 role :app, %w(deploy@188.166.60.176)
 role :web, %w(deploy@188.166.60.176)
